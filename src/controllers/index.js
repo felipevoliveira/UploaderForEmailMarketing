769b4abc933386033a3a937e977e5d5451b7564c
Felipe V. Oliveira <felipe_ld@hotmail.com>
2020-12-17 23:06:01 -0300

    Evolução da interface grafica

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -8,3 +8,8 @@ document.getElementById("copiar").addEventListener("click", function(){
     clipboard.writeText(document.getElementById("link").value, 'selection')
     
 });
+
+function upload(){
+    alert("valeu");
+    console.log("Deu certo!!!!!!!!!!!!");
+}
